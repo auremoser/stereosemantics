7521a4702b74b5db57b76cb30a9fbbb3d4fc6153
Julian Burgess <aubergene@gmail.com>
2014-06-08 23:06:17 -0400

    Script to set the YAML front matter from the MP3 and sets the MP3's tags to the episode, nifty!

---
+++ _scripts/update-mp3-info.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require "mp3info"
+require "yaml"
+
+files = Dir.glob(__dir__ + "/../_posts/*")
+
+files.each do |file|
+  puts file
+
+  f = open(file).read
+
+  content = f.split("---").last
+
+  data = YAML.load(f)
+
+  mp3_file = "mp3"
+  mp3_file << "/" << data["categories"].sub("season", "s")
+  mp3_file << "/e" << data["episode"].to_s.rjust(2, "0")
+  mp3_file << ".mp3"
+
+  next unless File.exists? mp3_file
+
+  Mp3Info.open(mp3_file) do |mp3|
+    # quick hack to format seconds to time
+    data["duration"] = Time.at(mp3.length).gmtime.strftime("%R:%S")
+    data["length"] = File.size(mp3_file)
+
+    mp3.tag.title = data["title"]
+    mp3.tag.artist = "Stereo Semantics"
+    mp3.tag.tracknum = data["episode"].to_i
+    mp3.tag.album = "Season #{data["categories"][/\d+/].to_i}"
+  end
+
+  # p data
+  # p content
+
+  open(file, "w") do |f|
+    f << data.to_yaml
+    f << "---"
+    f << content
+  end
+
+end
